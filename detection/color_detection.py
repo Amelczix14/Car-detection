5a87378a9c03f43aab913d859e8819f8d59a21f7
maadZia <mbernatagh@gmail.com>
2025-06-05 19:54:45 +0200

    color detection upgraded

--- detection/color_detection.py
+++ detection/color_detection.py
@@ -15,25 +15,35 @@ class ColorDetection:
 
         h, s, v = cv2.split(img_hsv)
 
-        # czerwony najwyższy priorytet
+        # czerwony – najwyższy priorytet
         red_mask1 = cv2.inRange(img_hsv, (0, 70, 50), (10, 255, 255))
         red_mask2 = cv2.inRange(img_hsv, (160, 70, 50), (180, 255, 255))
         red_mask = cv2.bitwise_or(red_mask1, red_mask2)
 
-        if cv2.countNonZero(red_mask) > (0.05 * red_mask.size):  # minimum 5% pikseli
+        if cv2.countNonZero(red_mask) > (0.05 * red_mask.size):
             return 'czerwony'
 
-        # neutralne kolory: biały, szary, czarny
-        neutral_mask = (s < 60) & (v > 40) & (v < 230)
-        neutral_pixels = v[neutral_mask]
+        # neutralne – czarny, biały, szary
+        neutral_mask = (s < 60)
+        s_neutral = s[neutral_mask]
+        v_neutral = v[neutral_mask]
 
-        if len(neutral_pixels) > 0:
-            median_v = np.median(neutral_pixels)
-            if median_v > 180:
-                return 'biały'
-            elif median_v < 70:
+        if len(v_neutral) > 0:
+            mean_v = np.mean(v_neutral)
+            std_v = np.std(v_neutral)
+            median_v = np.median(v_neutral)
+
+            # Czarny: niska jasność i bardzo niskie nasycenie
+            if mean_v < 90 and np.mean(s_neutral) < 40:
                 return 'czarny'
-            else:
+
+            # Biały: bardzo wysoka jasność
+            if mean_v > 200:
+                return 'biały'
+
+            # Szary: średnia jasność + niskie nasycenie
+            if 110 < mean_v < 200:
                 return 'szary'
 
         return 'unknown'
+
